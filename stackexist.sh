5ab5f50459fa63901d974b249fe92438e915ac76
mmanoj880 <75726986+mmanoj880@users.noreply.github.com>
2021-01-20 11:33:13 +0530

    Add files via upload

--- stackexist.sh
+++ stackexist.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 sname=BaseInfra
-if (aws cloudformation wait stack-exists --stack-name $sname --region ${region})
+REGION=us-east-2
+if aws cloudformation wait stack-exists --stack-name ${sname} --region ${REGION}
 then
+    do
     echo "Updating the existing stack"
-    aws cloudformation update-stack --stack-name $sname --template-body file:///home/ec2-user/workspace/BaseInfraWM/Base-Infra/CSD-PARENT.yml --parameters file:///home/ec2-user/workspace/BaseInfraWM/Base-Infra/input-${DEPLOY_ENV}.json --capabilities CAPABILITY_NAMED_IAM --region ${REGION} --notification-arns arn:aws:sns:us-east-2:108970575545:CloudformationStatus
+    aws cloudformation update-stack --stack-name ${sname} --template-body file:///home/ec2-user/workspace/BaseInfraWM/Base-Infra/CSD-PARENT.yml --parameters file:///home/ec2-user/workspace/BaseInfraWM/Base-Infra/input-${DEPLOY_ENV}.json --capabilities CAPABILITY_NAMED_IAM --region ${REGION} --notification-arns arn:aws:sns:us-east-2:108970575545:CloudformationStatus
+    done
 else
+    do
     echo " Creating a new stack "
-    aws cloudformation create-stack --stack-name $sname --template-body file:///home/ec2-user/workspace/BaseInfraWM/Base-Infra/CSD-PARENT.yml --parameters file:///home/ec2-user/workspace/BaseInfraWM/Base-Infra/input-${DEPLOY_ENV}.json --capabilities CAPABILITY_NAMED_IAM --region ${REGION} --notification-arns arn:aws:sns:us-east-2:108970575545:CloudformationStatus
+    aws cloudformation create-stack --stack-name ${sname} --template-body file:///home/ec2-user/workspace/BaseInfraWM/Base-Infra/CSD-PARENT.yml --parameters file:///home/ec2-user/workspace/BaseInfraWM/Base-Infra/input-${DEPLOY_ENV}.json --capabilities CAPABILITY_NAMED_IAM --region ${REGION} --notification-arns arn:aws:sns:us-east-2:108970575545:CloudformationStatus
+    done
 fi
